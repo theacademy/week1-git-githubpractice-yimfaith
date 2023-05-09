f4718935adfef052dfdbd9e4aa280e3d7826c535
usernameyimfaith <fyim192@gmail.com>
2023-05-09 11:19:24 -0700

    added Faith and stars java files

---
+++ stars.java
@@ -0,0 +1,11 @@
+public class stars {
+    public static void main(String[] args) {
+        System.out.println("  *  \n * * \n*   *\n * * \n  *  \n");
+        System.out.println("  *  ");
+        System.out.println(" * * ");
+        System.out.println("*   *");
+        System.out.println(" * * ");
+        System.out.println("  *  ");
+    }
+
+}
